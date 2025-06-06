d1ebb42eff65b4c338f8a180121f8757623df0b3
Abdullh1111 <abdullah4474032@gmail.com>
2025-06-06 19:36:59 +0600

    setup prisma for tickets

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/src/auth.module';
 import { PrismaModule } from './libs/prisma/prisma.module';
+import { TicketsModule } from './tickets/tickets.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule],
+  imports: [AuthModule, PrismaModule, TicketsModule],
   controllers: [],
   providers: [],
 })
